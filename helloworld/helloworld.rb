388cfad37abf4a0e99279f05d11cc72019accd7a
Ryan Prior <ryanprior@gmail.com>
2018-05-10 18:30:18 -0500

    Adds alternate version of demo that works with Conjur Enterprise

--- helloworld/helloworld.rb
+++ helloworld/helloworld.rb
@@ -8,9 +8,13 @@ secret = SecureRandom.hex(20)
 
 conjur_username = ENV['CONJUR_AUTHN_LOGIN']
 conjur_api_key = ENV['CONJUR_AUTHN_API_KEY']
+conjur_account = ENV['CONJUR_ACCOUNT']
+conjur_cert = ENV['CONJUR_CERT_FILE']
+
+OpenSSL::SSL::SSLContext::DEFAULT_CERT_STORE.add_file conjur_cert if conjur_cert
 
 api = Conjur::API.new_from_key conjur_username, conjur_api_key
-api.resource("demo:variable:helloworld-secret").add_value secret
+api.resource("#{conjur_account}:variable:helloworld-secret").add_value secret
 
 use Rack::TokenAuth do |token, options, env|
   token == secret
